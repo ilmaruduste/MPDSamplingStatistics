6bbfa9b52d2c6247359b88c843349f64d7e89554
Ilmar Uduste <ilmaruduste@wa03-0223a.wifi.kn.vutbr.cz>
2021-09-28 08:45:36 +0200

    Add building of SQL query for input data selection

--- src/input_data_selector.py
+++ src/input_data_selector.py
@@ -2,15 +2,26 @@ import pandas
 
 class InputDataSelector:
     
-    def __init__(self, data_schemas, data_types, table_names, indicators):
-        self.data_schemas = data_schemas
-        self.data_types = data_types
-        self.table_names = table_names
-        self.indicators = indicators
+    def __init__(self, conf, table_name):
+        self.data_schemas = conf["INPUT DATA"]["DATA SCHEMAS"]
+        self.data_types = conf["INPUT DATA"]["CORRESPONDING TYPES"]
+        self.indicators = conf["INPUT DATA"]["INDICATORS"]
+        self.table_name = table_name          # We have one specific table name per InputDataSelector instance
         self.query = None
 
     def create_sql_query(self):
-        return True
+        sql_query = ""
 
-    def create_pandas_df_from_query(self, connection):
+        for i, schema, type in enumerate(zip(self.data_schemas, self.data_types)):
+            sql_query += '''SELECT *, "{ftype}" AS data_type FROM {fschema}.{ftablename}\n'''.format(fschema = schema, ftype = type, ftablename = self.table_name)
+
+            if (i < len(self.data_schemas) - 1):
+                sql_query += "UNION\n"
+
+        self.query = sql_query
+        
+    def get_sql_query(self):
+        return self.query
+
+    def get_pandas_df_from_query(self, connection):
         return pandas.read_sql_query(self.query, connection)

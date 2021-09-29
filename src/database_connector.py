bbd1f900cdf1fce33220bc874f4a684b304039b5
Ilmar Uduste <ilmaruduste@wa03-0223a.wifi.kn.vutbr.cz>
2021-09-29 12:31:08 +0200

    Implement tests for database connector

--- src/database_connector.py
+++ src/database_connector.py
@@ -2,13 +2,15 @@ import psycopg2
 
 class DatabaseConnector:
 
-    def __init__(self, conf):
+    def __init__(self):
+        self.db_connection = None
+
+    def load_conf(self, conf):
         self.db_host = conf['DATABASE CONNECTION']['DB HOST']
         self.db_name = conf['DATABASE CONNECTION']['DB NAME']
         self.db_username = conf['DATABASE CONNECTION']['DB USERNAME']
         self.db_password = conf['DATABASE CONNECTION']['DB PASSWORD']
         self.db_port = conf['DATABASE CONNECTION']['DB PORT']
-        self.db_connection = None
 
     def connect_to_db(self):
         self.db_connection = psycopg2.connect(
---
+++ tests/database_connector/test_database_connector.py
@@ -0,0 +1,29 @@
+# Read up on unittests: https://docs.python.org/3/library/unittest.html
+# https://realpython.com/python-testing/
+
+
+from src import database_connector
+import unittest
+import yaml
+import time
+
+class TestDatabaseConnector(unittest.TestCase):
+    def setUp(self):
+        self.dc = database_connector.DatabaseConnector()
+
+    def test_load_config(self):
+        conf = yaml.safe_load(open("./tests/database_connector/test_database_connector_conf.yaml", "r"))
+        self.dc.load_conf(conf)
+        self.assertEqual(self.dc.db_host, "localhost")
+        self.assertEqual(self.dc.db_name, "dummy_name")
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestDatabaseConnector('setUp'))
+    suite.addTest(TestDatabaseConnector('test_load_config'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())

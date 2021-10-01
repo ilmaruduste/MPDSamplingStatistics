efee51e8e05a18d8c5218824ec0032f1305b08e7
Ilmar Uduste <ilmaruduste@dhcpe049.fit.vutbr.cz>
2021-10-01 10:26:23 +0200

    Add combination coverage calculation and tests

--- src/results_row_creator.py
+++ src/results_row_creator.py
@@ -12,13 +12,18 @@ class ResultsRowCreator:
         self.original_data = original_data
         self.comparison_data = comparison_data
 
+    def calculate_combination_coverage(self, filter_name = None, filter_value = None):
+
+        if (filter_name == None and filter_value == None):
+            return round(len(self.comparison_data)/len(self.original_data), 4)
+        else:
+            return(round(get_division_of_lengths_filters(self.original_data, self.comparison_data, filter_name, filter_value), 4))
+
 class DomInbResultsRowCreator(ResultsRowCreator):
 
     def __init__(self, original_data, comparison_data):
         super().__init__(original_data, comparison_data)
 
-    def calculate_combination_coverage(self, filter_name, filter_value):
-        return(round(get_division_of_lengths_filters(self.original_data, self.comparison_data, filter_name, filter_value), 4))
 
 class OutbResultsRowCreator(ResultsRowCreator):
 
--- test/test_results_row_creator.py
+++ test/test_results_row_creator.py
@@ -11,18 +11,39 @@ class TestResultsRowCreator(unittest.TestCase):
         self.dirrc = results_row_creator.DomInbResultsRowCreator(None, None)
         self.outbrrc = results_row_creator.OutbResultsRowCreator(None, None)
 
-    def test_combination_coverage_with_filter_1(self):
-        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2]}),
+    def test_combination_coverage_without_filter(self):
+        self.outbrrc = results_row_creator.OutbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
                                                                 comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
-        # self.dirrc.original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2]})
-        # self.dirrc.comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]})
 
-        self.assertEquals(self.dirrc.calculate_combination_coverage('dummy_category', 1), 0.75)
-        # self.assertEquals(self.dirrc.calculate_combination_coverage('dummy_category', 2), 1)
+        self.assertEqual(self.outbrrc.calculate_combination_coverage(), round(5/7, 4))
+
+    def test_combination_coverage_with_filter_1(self):
+        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
+                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
+
+        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 1), 0.75)
+
+    def test_combination_coverage_with_filter_2(self):
+        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
+                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
+        
+        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 2), 1)
+
+    def test_combination_coverage_with_filter_3(self):
+        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
+                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
+
+        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 3), 0)
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestResultsRowCreator('setUp'))
+    
+    suite.addTest(TestResultsRowCreator('test_combination_coverage_without_filter'))
+    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_1'))
+    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_2'))
+    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_3'))
+
     return suite
 
 if __name__ == '__main__':

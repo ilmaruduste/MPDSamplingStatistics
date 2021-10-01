b837d42a96448487157e3c19fa009236ce5d683d
Ilmar Uduste <ilmaruduste@dhcpe049.fit.vutbr.cz>
2021-10-01 11:42:08 +0200

    Add ks and tests

--- src/results_row_creator.py
+++ src/results_row_creator.py
@@ -1,4 +1,5 @@
 import pandas
+from scipy import stats
 
 def get_len_filtered_data(data, filter_name, filter_value):
     return len(data[data[filter_name] == filter_value])
@@ -13,11 +14,20 @@ class ResultsRowCreator:
         self.comparison_data = comparison_data
 
     def calculate_combination_coverage(self, filter_name = None, filter_value = None):
-
         if (filter_name == None and filter_value == None):
             return round(len(self.comparison_data)/len(self.original_data), 4)
         else:
             return(round(get_division_of_lengths_filters(self.original_data, self.comparison_data, filter_name, filter_value), 4))
+
+    def calculate_ks_test(self, indicator_name, filter_name = None, filter_value = None):
+        # Returns a tuple. 
+        # First value is the KS statistic (D)
+        # Second value is the likelihood (p-value) that these two datas are from the same distribution
+        if (filter_name == None and filter_value == None):
+            return stats.kstest(self.original_data[indicator_name], self.comparison_data[indicator_name])
+        else:
+            return stats.kstest(self.original_data[self.original_data[filter_name] == filter_value][indicator_name],
+                                self.comparison_data[self.comparison_data[filter_name] == filter_value][indicator_name])
 
 class DomInbResultsRowCreator(ResultsRowCreator):
 
--- test/test_results_row_creator.py
+++ test/test_results_row_creator.py
@@ -7,38 +7,42 @@ import pandas as pd
 
 class TestResultsRowCreator(unittest.TestCase):
     def setUp(self):
-        self.rrc = results_row_creator.ResultsRowCreator(None, None)
+        self.rrc = results_row_creator.ResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
+                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
         self.dirrc = results_row_creator.DomInbResultsRowCreator(None, None)
         self.outbrrc = results_row_creator.OutbResultsRowCreator(None, None)
 
     def test_combination_coverage_without_filter(self):
-        self.outbrrc = results_row_creator.OutbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
-                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
-
-        self.assertEqual(self.outbrrc.calculate_combination_coverage(), round(5/7, 4))
+        self.assertEqual(self.rrc.calculate_combination_coverage(), round(5/7, 4))
 
     def test_combination_coverage_with_filter_1(self):
-        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
-                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
-
-        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 1), 0.75)
+        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 1), 0.75)
 
     def test_combination_coverage_with_filter_2(self):
-        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
-                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
-        
-        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 2), 1)
+        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 2), 1)
 
     def test_combination_coverage_with_filter_3(self):
-        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
-                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
+        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 3), 0)
 
-        self.assertEqual(self.dirrc.calculate_combination_coverage('dummy_category', 3), 0)
+    def test_ks_test_0(self):
+        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
+        self.rrc.comparison_data['dummy_indicator'] = 0
+        self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator')[1], 0, places = 2)
+
+    def test_ks_test_1(self):
+        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
+        self.rrc.comparison_data['dummy_indicator'] = [10,20,30,50,70]
+        self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator')[1], 1, places = 2)
+
+    def test_ks_test_1_filter(self):
+        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
+        self.rrc.comparison_data['dummy_indicator'] = [10,20,30,0,0]
+        self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator', filter_name='dummy_category', filter_value=1)[1], 1, places = 2)
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestResultsRowCreator('setUp'))
-    
+
     suite.addTest(TestResultsRowCreator('test_combination_coverage_without_filter'))
     suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_1'))
     suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_2'))

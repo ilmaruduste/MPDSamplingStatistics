80e0545195edbfd683a9c1e1ed7f20a2606b61a8
Ilmar Uduste <ilmaruduste@wa03-0223a.wifi.kn.vutbr.cz>
2021-10-04 12:39:27 +0200

    Add indicator means, dataframe joins and tests

--- src/results_row_creator.py
+++ src/results_row_creator.py
@@ -1,11 +1,9 @@
 import pandas
 from scipy import stats
+import pandas as pd
 
 def get_len_filtered_data(data, filter_name, filter_value):
     return len(data[data[filter_name] == filter_value])
-
-def get_division_of_lengths_filters(original_data, comparison_data, filter_name, filter_value):
-    return get_len_filtered_data(comparison_data, filter_name, filter_value)/get_len_filtered_data(original_data, filter_name, filter_value)
 
 class ResultsRowCreator:
 
@@ -13,11 +11,17 @@ class ResultsRowCreator:
         self.original_data = original_data
         self.comparison_data = comparison_data
 
-    def calculate_combination_coverage(self, filter_name = None, filter_value = None):
+    def get_filtered_orig_data(self, filter_name, filter_value):
+        return self.original_data[self.original_data[filter_name] == filter_value]
+
+    def get_division_of_lengths_filters(self, filter_name, filter_value):
+        return get_len_filtered_data(self.comparison_data, filter_name, filter_value)/get_len_filtered_data(self.original_data, filter_name, filter_value)
+
+    def calculate_combination_coverage(self, filter_name = None, filter_value = None, round_decimal_places = 4):
         if (filter_name == None and filter_value == None):
             return round(len(self.comparison_data)/len(self.original_data), 4)
         else:
-            return(round(get_division_of_lengths_filters(self.original_data, self.comparison_data, filter_name, filter_value), 4))
+            return(round(self.get_division_of_lengths_filters(filter_name, filter_value), round_decimal_places))
 
     def calculate_ks_test(self, indicator_name, filter_name = None, filter_value = None):
         # Returns a tuple. 
@@ -26,8 +30,23 @@ class ResultsRowCreator:
         if (filter_name == None and filter_value == None):
             return stats.kstest(self.original_data[indicator_name], self.comparison_data[indicator_name])
         else:
-            return stats.kstest(self.original_data[self.original_data[filter_name] == filter_value][indicator_name],
+            return stats.kstest(self.get_filtered_orig_data(filter_name, filter_value)[indicator_name],
                                 self.comparison_data[self.comparison_data[filter_name] == filter_value][indicator_name])
+
+    def calculate_original_indicator_mean(self, indicator_name, filter_name = None, filter_value = None, round_decimal_places = 4):
+        if (filter_name == None and filter_value == None):
+            return round(self.original_data[indicator_name].mean(), round_decimal_places)
+        else:
+            return round(self.get_filtered_orig_data(filter_name, filter_value)[indicator_name].mean(), round_decimal_places)
+
+    def calculate_original_indicator_mad(self, indicator_name, filter_name = None, filter_value = None, round_decimal_places = 4):
+        if (filter_name == None and filter_value == None):
+            return round(stats.median_abs_deviation(self.original_data[indicator_name]), round_decimal_places)
+        else:
+            return round(stats.median_abs_deviation(self.get_filtered_orig_data(filter_name, filter_value)[indicator_name]), round_decimal_places)
+
+    def join_orig_comp_data(self, join_categories):
+        self.joined_data = pd.merge(self.original_data, self.comparison_data, how = "inner", on = join_categories)
 
 class DomInbResultsRowCreator(ResultsRowCreator):
 
--- test/test_results_row_creator.py
+++ test/test_results_row_creator.py
@@ -5,10 +5,18 @@ from src import results_row_creator
 import unittest
 import pandas as pd
 
+
 class TestResultsRowCreator(unittest.TestCase):
     def setUp(self):
-        self.rrc = results_row_creator.ResultsRowCreator(original_data = pd.DataFrame({'dummy_category':[1,1,1,1,2,2,3]}),
-                                                                comparison_data = pd.DataFrame({'dummy_category':[1,1,1,2,2]}))
+        self.rrc = results_row_creator.ResultsRowCreator(
+            original_data = pd.DataFrame(
+                {'dummy_date':['2021-01-01', '2021-02-01', '2021-03-01', '2021-04-01', '2021-01-01','2021-02-01','2021-01-01'],
+                'dummy_category':[1,1,1,1,2,2,3], 
+                'dummy_indicator':[10,20,30,40,50,60,70]}),
+            comparison_data = pd.DataFrame(
+                {'dummy_date':['2021-01-01', '2021-02-01', '2021-03-01', '2021-01-01','2021-02-01'],
+                'dummy_category':[1,1,1,2,2],
+                'dummy_indicator': [10,20,30,50,70]}))
         self.dirrc = results_row_creator.DomInbResultsRowCreator(None, None)
         self.outbrrc = results_row_creator.OutbResultsRowCreator(None, None)
 
@@ -25,19 +33,35 @@ class TestResultsRowCreator(unittest.TestCase):
         self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 3), 0)
 
     def test_ks_test_0(self):
-        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
         self.rrc.comparison_data['dummy_indicator'] = 0
         self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator')[1], 0, places = 2)
 
     def test_ks_test_1(self):
-        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
-        self.rrc.comparison_data['dummy_indicator'] = [10,20,30,50,70]
         self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator')[1], 1, places = 2)
 
     def test_ks_test_1_filter(self):
-        self.rrc.original_data['dummy_indicator'] = [10,20,30,40,50,60,70]
         self.rrc.comparison_data['dummy_indicator'] = [10,20,30,0,0]
         self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator', filter_name='dummy_category', filter_value=1)[1], 1, places = 2)
+
+    def test_original_indicator_mean(self):
+        self.assertEqual(self.rrc.calculate_original_indicator_mean('dummy_indicator'), 40)
+
+    def test_original_indicator_mean_filter(self):
+        self.assertEqual(self.rrc.calculate_original_indicator_mean('dummy_indicator', filter_name = 'dummy_category', filter_value = 1), 25)
+
+    def test_original_indicator_mad(self):
+        self.assertEqual(self.rrc.calculate_original_indicator_mad('dummy_indicator'), 20)
+
+    def test_left_join_datasets(self):
+        join_categories = ['dummy_date', 'dummy_category']
+        result = pd.DataFrame({
+            'dummy_date':['2021-01-01', '2021-02-01', '2021-03-01','2021-01-01','2021-02-01'],
+            'dummy_category':[1,1,1,2,2], 
+            'dummy_indicator_x':[10,20,30,50,60],
+            'dummy_indicator_y':[10,20,30,50,70]})
+        self.rrc.join_orig_comp_data(join_categories)
+        pd.testing.assert_frame_equal(result, self.rrc.joined_data, )
+
 
 def suite():
     suite = unittest.TestSuite()

a9290734894a5627c6103d7527c42cf2b98af97a
Ilmar Uduste <ilmaruduste@wa03-0223a.wifi.kn.vutbr.cz>
2021-10-06 11:18:40 +0200

    Add percentage error metrics and tests

--- src/results_row_creator.py
+++ src/results_row_creator.py
@@ -1,6 +1,7 @@
 import pandas
 from scipy import stats
 import pandas as pd
+import numpy as np
 
 def get_len_filtered_data(data, filter_name, filter_value):
     return len(data[data[filter_name] == filter_value])
@@ -13,6 +14,20 @@ class ResultsRowCreator:
 
     def get_filtered_orig_data(self, filter_name, filter_value):
         return self.original_data[self.original_data[filter_name] == filter_value]
+
+    def get_filtered_comp_data(self, filter_name, filter_value):
+        return self.comparison_data[self.comparison_data[filter_name] == filter_value]
+
+    def get_filtered_joined_data(self, filter_name, filter_value):
+        return self.joined_data[self.joined_data[filter_name] == filter_value]
+
+    def get_joined_data_indicators(self, indicator_name_x, indicator_name_y, filter_name = None, filter_value = None):
+        if (filter_name == None and filter_value == None):
+            return zip(self.joined_data[indicator_name_x], 
+                        self.joined_data[indicator_name_y])
+        else:
+            return zip(self.get_filtered_joined_data(filter_name, filter_value)[indicator_name_x], 
+                        self.get_filtered_joined_data(filter_name, filter_value)[indicator_name_y])
 
     def get_division_of_lengths_filters(self, filter_name, filter_value):
         return get_len_filtered_data(self.comparison_data, filter_name, filter_value)/get_len_filtered_data(self.original_data, filter_name, filter_value)
@@ -40,6 +55,7 @@ class ResultsRowCreator:
             return round(self.get_filtered_orig_data(filter_name, filter_value)[indicator_name].mean(), round_decimal_places)
 
     def calculate_original_indicator_mad(self, indicator_name, filter_name = None, filter_value = None, round_decimal_places = 4):
+        # MAD - mean_absolute_deviation
         if (filter_name == None and filter_value == None):
             return round(stats.median_abs_deviation(self.original_data[indicator_name]), round_decimal_places)
         else:
@@ -47,6 +63,21 @@ class ResultsRowCreator:
 
     def join_orig_comp_data(self, join_categories):
         self.joined_data = pd.merge(self.original_data, self.comparison_data, how = "inner", on = join_categories)
+
+    def calculate_absolute_percentage_error_metrics(self, indicator_name, filter_name = None, filter_value = None, round_decimal_places = 4):
+        # This function assumes that self.joined_data exists (i.e. join_orig_comp_data() has been done)
+        # The function returns an array
+        # First value is MAPE - Mean Absolute Percentage Error
+        # The second value is MAD - Median Absolute Deviation
+        x, y = indicator_name + "_x", indicator_name + "_y"
+
+        if (filter_name == None and filter_value == None):
+            percentage_errors = np.array([abs(orig_ind - comp_ind)/orig_ind*100 for orig_ind, comp_ind in self.get_joined_data_indicators(x, y)])
+        else:
+            percentage_errors = np.array([abs(orig_ind - comp_ind)/orig_ind*100 for orig_ind, comp_ind in self.get_joined_data_indicators(x, y, filter_name, filter_value)])
+        
+        return np.round([percentage_errors.mean(), stats.median_abs_deviation(percentage_errors)], round_decimal_places)
+
 
 class DomInbResultsRowCreator(ResultsRowCreator):
 
--- test/test_results_row_creator.py
+++ test/test_results_row_creator.py
@@ -20,6 +20,14 @@ class TestResultsRowCreator(unittest.TestCase):
         self.dirrc = results_row_creator.DomInbResultsRowCreator(None, None)
         self.outbrrc = results_row_creator.OutbResultsRowCreator(None, None)
 
+        join_categories = ['dummy_date', 'dummy_category']
+        result = pd.DataFrame({
+            'dummy_date':['2021-01-01', '2021-02-01', '2021-03-01','2021-01-01','2021-02-01'],
+            'dummy_category':[1,1,1,2,2], 
+            'dummy_indicator_x':[10,20,30,50,60],
+            'dummy_indicator_y':[10,20,30,50,70]})
+        self.rrc.join_orig_comp_data(join_categories)
+
     def test_combination_coverage_without_filter(self):
         self.assertEqual(self.rrc.calculate_combination_coverage(), round(5/7, 4))
 
@@ -52,28 +60,39 @@ class TestResultsRowCreator(unittest.TestCase):
     def test_original_indicator_mad(self):
         self.assertEqual(self.rrc.calculate_original_indicator_mad('dummy_indicator'), 20)
 
-    def test_left_join_datasets(self):
-        join_categories = ['dummy_date', 'dummy_category']
-        result = pd.DataFrame({
-            'dummy_date':['2021-01-01', '2021-02-01', '2021-03-01','2021-01-01','2021-02-01'],
-            'dummy_category':[1,1,1,2,2], 
-            'dummy_indicator_x':[10,20,30,50,60],
-            'dummy_indicator_y':[10,20,30,50,70]})
-        self.rrc.join_orig_comp_data(join_categories)
-        pd.testing.assert_frame_equal(result, self.rrc.joined_data, )
+    def test_mean_absolute_percentage_error_0(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [10,20,30,50,60]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator')[0], 0)
 
+    def test_mean_absolute_percentage_error_50(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [5,10,15,25,30]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator')[0], 50)
 
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestResultsRowCreator('setUp'))
+    def test_mean_absolute_percentage_error_100(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [0,0,0,0,0]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator')[0], 100)
 
-    suite.addTest(TestResultsRowCreator('test_combination_coverage_without_filter'))
-    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_1'))
-    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_2'))
-    suite.addTest(TestResultsRowCreator('test_combination_coverage_with_filter_3'))
+    def test_mean_absolute_percentage_error_50_filter(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [5,10,15,50,60]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', filter_name = 'dummy_category', filter_value = 1)[0], 50)
 
-    return suite
+    def test_mean_absolute_percentage_error_200_filter(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [5,10,15,150,180]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', filter_name = 'dummy_category', filter_value = 2)[0], 200)
+
+    def test_mad_absolute_percentage_error_0(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [10,20,30,50,60]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator')[1], 0)
+
+    def test_mad_absolute_percentage_error_50(self):
+        self.rrc.joined_data['dummy_indicator_y'] = [10,20,15,100,120]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator')[1], 50)
+
+    def test_mad_absolute_percentage_error_0_filter(self):
+        # This test works since it's MEDIAN and not MEAN absolute deviation
+        self.rrc.joined_data['dummy_indicator_y'] = [4,20,30,100,120]
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', filter_name = 'dummy_category', filter_value = 1)[1], 0)
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
-    runner.run(suite())
+    # runner.run(suite())

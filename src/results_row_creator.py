3101f1a16cf4f51c30c0f7cf3f738bff6efb44d8
Ilmar Uduste <ilmaruduste@gmail.com>
2021-11-10 15:26:39 +0100

    Add possibility to use multiple groups when calculating grouped statistics

--- src/results_row_creator.py
+++ src/results_row_creator.py
@@ -1,9 +1,7 @@
 from scipy import stats
 import pandas as pd
 import numpy as np
-
-def get_len_grouped_data(data, group_name, group_value):
-    return len(data[data[group_name] == group_value])
+import itertools
 
 class ResultsRowCreator:
 
@@ -13,14 +11,32 @@ class ResultsRowCreator:
         self.join_categories = join_categories
         self.comparison_data_coef = comparison_data_coef
 
-    def get_grouped_orig_data(self, group_name, group_value):
-        return self.original_data[self.original_data[group_name] == group_value]
+    def get_grouped_orig_data(self, group_name, group_value_array):
+        grouped_data = self.original_data.copy()
+        
+        # This is done so multiple groups could be used
+        for i, name in enumerate(group_name):
+            grouped_data = grouped_data[grouped_data[name] == group_value_array[i]]
 
-    def get_grouped_comp_data(self, group_name, group_value):
-        return self.comparison_data[self.comparison_data[group_name] == group_value]
+        return grouped_data
 
-    def get_grouped_joined_data(self, group_name, group_value):
-        return self.joined_data[self.joined_data[group_name] == group_value]
+    def get_grouped_comp_data(self, group_name, group_value_array):
+        grouped_data = self.comparison_data.copy()
+
+        # This is done so multiple groups could be used
+        for i, name in enumerate(group_name):
+            grouped_data = grouped_data[grouped_data[name] == group_value_array[i]]
+            
+        return grouped_data
+
+    def get_grouped_joined_data(self, group_name, group_value_array):
+        grouped_data = self.joined_data.copy()
+
+        # This is done so multiple groups could be used
+        for i, name in enumerate(group_name):
+            grouped_data = grouped_data[grouped_data[name] == group_value_array[i]]
+            
+        return grouped_data
 
     def get_joined_data_indicators(self, indicator_name_x, indicator_name_y, group_name = None, group_value = None):
         if (group_name == None and group_value == None):
@@ -32,7 +48,8 @@ class ResultsRowCreator:
 
     def get_division_of_lengths_groups(self, group_name, group_value):
         try:
-            return get_len_grouped_data(self.comparison_data, group_name, group_value)/get_len_grouped_data(self.original_data, group_name, group_value)
+            # return get_len_grouped_data(self.comparison_data, group_name, group_value)/get_len_grouped_data(self.original_data, group_name, group_value)
+            return len(self.get_grouped_comp_data(group_name, group_value))/len(self.get_grouped_orig_data(group_name, group_value))
         except(ZeroDivisionError):
             return np.NaN
 
@@ -51,7 +68,7 @@ class ResultsRowCreator:
                 return stats.kstest(self.original_data[indicator_name], self.comparison_data[indicator_name])
             else:
                 return stats.kstest(self.get_grouped_orig_data(group_name, group_value)[indicator_name],
-                                    self.comparison_data[self.comparison_data[group_name] == group_value][indicator_name])
+                                    self.get_grouped_comp_data(group_name, group_value)[indicator_name])
         except(ValueError):
             return (np.NaN, np.NaN)
 
@@ -126,31 +143,77 @@ class DomInbResultsRowCreator(ResultsRowCreator):
         return results_dict
 
     def get_row_of_statistics_with_group(self, table_name, data_type, indicator_name, group_name, group_value_array):
+        
+        group_value_product_array = list(itertools.product(*group_value_array))
+        group_combination_cnt = sum(1 for x in group_value_product_array)
+
         self.multiply_comp_with_coef(indicator_name)
         self.join_orig_comp_data(self.join_categories)
-        combination_coverage_array = [self.calculate_combination_coverage(group_name, group_value) for group_value in group_value_array]
-        ks_test_array = [self.calculate_ks_test(indicator_name, group_name, group_value) for group_value in group_value_array]
-        indicator_mean_array = [self.calculate_original_indicator_mean(indicator_name, group_name, group_value) for group_value in group_value_array]
-        indicator_mad_array = [self.calculate_original_indicator_mad(indicator_name, group_name, group_value) for group_value in group_value_array]
-        ape_metrics_array = np.array([self.calculate_absolute_percentage_error_metrics(indicator_name, group_name, group_value) for group_value in group_value_array]).T
-        ale_metrics_array = np.array([self.calculate_absolute_logarithmic_error_metrics(indicator_name, group_name, group_value) for group_value in group_value_array]).T
-
-        results_dict = {
-            'table_name': np.repeat([table_name], len(group_value_array))
-            , 'data_type': np.repeat([data_type], len(group_value_array))
-            , 'indicator': np.repeat([indicator_name], len(group_value_array))
-            , group_name: group_value_array
-            , 'combination_coverage': combination_coverage_array
-            , 'ks_test_D': np.array(ks_test_array).T[0]
-            , 'ks_test_p': np.array(ks_test_array).T[1]
-            , 'indicator_mean': indicator_mean_array
-            , 'indicator_mad': indicator_mad_array
-            , 'APE_mean': ape_metrics_array[0]
-            , 'APE_mad': ape_metrics_array[1]
-            , 'ALE_mean': ale_metrics_array[0]
-            , 'ALE_mad': ale_metrics_array[1]
-        }
         
+        combination_coverage_array = [self.calculate_combination_coverage(group_name, group_value_combination) for group_value_combination in group_value_product_array]
+        ks_test_array = [self.calculate_ks_test(indicator_name, group_name, group_value_combination) for group_value_combination in group_value_product_array]
+        indicator_mean_array = [self.calculate_original_indicator_mean(indicator_name, group_name, group_value_combination) for group_value_combination in group_value_product_array]
+        indicator_mad_array = [self.calculate_original_indicator_mad(indicator_name, group_name, group_value_combination) for group_value_combination in group_value_product_array]
+        ape_metrics_array = np.array([self.calculate_absolute_percentage_error_metrics(indicator_name, group_name, group_value_combination) for group_value_combination in group_value_product_array]).T
+        ale_metrics_array = np.array([self.calculate_absolute_logarithmic_error_metrics(indicator_name, group_name, group_value_combination) for group_value_combination in group_value_product_array]).T
+        # group_value_combination_array = [group_value_combination for group_value_combination in group_value_product_array]
+
+        # For getting the correct number of rows into the grouping variables
+        group_value_combination_array = []
+        for length in range(len(group_value_array)):
+            one_group_array = [combination[length] for combination in group_value_product_array]
+            group_value_combination_array.append(one_group_array)
+
+        # 2021-11-10: This is how column assignment was done before using multiple groups was introduced
+        # results_dict = {
+        #     'table_name': np.repeat([table_name], len(group_value_array))
+        #     , 'data_type': np.repeat([data_type], len(group_value_array))
+        #     , 'indicator': np.repeat([indicator_name], len(group_value_array))
+        #     # TODO: unhashable type 'list'!
+        #     , group_name: group_value_array
+        #     , 'combination_coverage': combination_coverage_array
+        #     , 'ks_test_D': np.array(ks_test_array).T[0]
+        #     , 'ks_test_p': np.array(ks_test_array).T[1]
+        #     , 'indicator_mean': indicator_mean_array
+        #     , 'indicator_mad': indicator_mad_array
+        #     , 'APE_mean': ape_metrics_array[0]
+        #     , 'APE_mad': ape_metrics_array[1]
+        #     , 'ALE_mean': ale_metrics_array[0]
+        #     , 'ALE_mad': ale_metrics_array[1]
+        # }
+        
+        columns = [
+            'table_name', 
+            'data_type', 
+            'indicator', 
+            *group_name, 
+            'combination_coverage',
+            'ks_test_D', 
+            'ks_test_p', 
+            'indicator_mean', 
+            'indicator_mad', 
+            'APE_mean', 
+            'APE_mad',
+            'ALE_mean', 
+            'ALE_mad']
+
+        column_values = [
+            np.repeat([table_name], group_combination_cnt), 
+            np.repeat([data_type], group_combination_cnt), 
+            np.repeat([indicator_name], group_combination_cnt), 
+            *group_value_combination_array, 
+            combination_coverage_array,
+            np.array(ks_test_array).T[0], 
+            np.array(ks_test_array).T[1], 
+            indicator_mean_array, 
+            indicator_mad_array,
+            ape_metrics_array[0], 
+            ape_metrics_array[1], 
+            ale_metrics_array[0], 
+            ale_metrics_array[1]]
+
+        results_dict = dict(zip(columns, column_values))
+
         return results_dict
 
     def get_row_of_statistics_without_group(self, table_name, data_type, indicator_name):
--- test/test_results_row_creator.py
+++ test/test_results_row_creator.py
@@ -41,13 +41,13 @@ class TestResultsRowCreator(unittest.TestCase):
         self.assertEqual(self.rrc.calculate_combination_coverage(), round(5/7, 4))
 
     def test_combination_coverage_with_group_1(self):
-        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 1), 0.75)
+        self.assertEqual(self.rrc.calculate_combination_coverage(['dummy_category'], [1]), 0.75)
 
     def test_combination_coverage_with_group_2(self):
-        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 2), 1)
+        self.assertEqual(self.rrc.calculate_combination_coverage(['dummy_category'], [2]), 1)
 
     def test_combination_coverage_with_group_3(self):
-        self.assertEqual(self.rrc.calculate_combination_coverage('dummy_category', 3), 0)
+        self.assertEqual(self.rrc.calculate_combination_coverage(['dummy_category'], [3]), 0)
 
     def test_ks_test_0(self):
         self.rrc.comparison_data['dummy_indicator'] = 0
@@ -58,13 +58,13 @@ class TestResultsRowCreator(unittest.TestCase):
 
     def test_ks_test_1_group(self):
         self.rrc.comparison_data['dummy_indicator'] = [10,20,30,0,0]
-        self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator', group_name='dummy_category', group_value=1)[1], 1, places = 2)
+        self.assertAlmostEqual(self.rrc.calculate_ks_test('dummy_indicator', group_name=['dummy_category'], group_value=[1])[1], 1, places = 2)
 
     def test_original_indicator_mean(self):
         self.assertEqual(self.rrc.calculate_original_indicator_mean('dummy_indicator'), 40)
 
     def test_original_indicator_mean_group(self):
-        self.assertEqual(self.rrc.calculate_original_indicator_mean('dummy_indicator', group_name = 'dummy_category', group_value = 1), 25)
+        self.assertEqual(self.rrc.calculate_original_indicator_mean('dummy_indicator', group_name = ['dummy_category'], group_value = [1]), 25)
 
     def test_original_indicator_mad(self):
         self.assertEqual(self.rrc.calculate_original_indicator_mad('dummy_indicator'), 20)
@@ -83,11 +83,11 @@ class TestResultsRowCreator(unittest.TestCase):
 
     def test_mean_absolute_percentage_error_50_group(self):
         self.rrc.joined_data['dummy_indicator_y'] = [5,10,15,50,60]
-        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = 'dummy_category', group_value = 1)[0], 50)
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = ['dummy_category'], group_value = [1])[0], 50)
 
     def test_mean_absolute_percentage_error_200_group(self):
         self.rrc.joined_data['dummy_indicator_y'] = [5,10,15,150,180]
-        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = 'dummy_category', group_value = 2)[0], 200)
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = ['dummy_category'], group_value = [2])[0], 200)
 
     def test_mad_absolute_percentage_error_0(self):
         self.rrc.joined_data['dummy_indicator_y'] = [10,20,30,50,60]
@@ -100,7 +100,7 @@ class TestResultsRowCreator(unittest.TestCase):
     def test_mad_absolute_percentage_error_0_group(self):
         # This test works since it's MEDIAN and not MEAN absolute deviation
         self.rrc.joined_data['dummy_indicator_y'] = [4,20,30,100,120]
-        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = 'dummy_category', group_value = 1)[1], 0)
+        self.assertEqual(self.rrc.calculate_absolute_percentage_error_metrics('dummy_indicator', group_name = ['dummy_category'], group_value = [1])[1], 0)
 
     def test_mean_ale_0(self):
         self.rrc.joined_data['dummy_indicator_y'] = [10,20,30,50,60]
@@ -137,7 +137,28 @@ class TestResultsRowCreator(unittest.TestCase):
         lau_level_array = [0,1,2,3]
 
         self.dirrc = results_row_creator.DomInbResultsRowCreator(original_df, comparison_df, join_categories)
-        print(self.dirrc.get_row_of_statistics('dummy_table', 'dummy_data', 'dummy_indicator', 'dummy_category', lau_level_array))
+        print(self.dirrc.get_row_of_statistics('dummy_table', 'dummy_data', 'dummy_indicator', ['dummy_category'], [lau_level_array]))
+
+    def test_dominb_statistics_2_groups(self):
+        join_categories = ['dummy_date', 'group1', 'group2']
+
+        original_df = pd.DataFrame(
+            {'dummy_date':['2021-01-01', '2021-01-01', '2021-02-01', '2021-03-01', '2021-04-01', '2021-01-01','2021-02-01','2021-01-01', '2021-02-01'],
+            'group1':[0,1,1,1,1,2,2,3,3], 
+            'group2':["a","a","b","a","b","a","b","a","b"], 
+            'dummy_indicator':[5,10,20,30,40,50,60,70,80]})
+
+        comparison_df = pd.DataFrame(
+            {'dummy_date':['2021-01-01', '2021-01-01', '2021-02-01', '2021-03-01', '2021-01-01','2021-02-01', '2021-01-01', '2021-02-01'],
+            'group1':[0,1,1,1,2,2,3,3],
+            'group2':["a","a","b","a","a","b","a","b"], 
+            'dummy_indicator': [5,10,20,30,50,70,80, 90]})
+        
+        group1_array = [0,1,2,3]
+        group2_array = ["a","b"]
+
+        self.dirrc = results_row_creator.DomInbResultsRowCreator(original_df, comparison_df, join_categories)
+        print(self.dirrc.get_row_of_statistics('dummy_table', 'dummy_data', 'dummy_indicator', ['group1', 'group2'], [group1_array, group2_array]))
 
     def test_dominb_without_group_statistics(self):
         join_categories = ['dummy_date', 'dummy_category']
